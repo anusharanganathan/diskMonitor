8657876da6290feca85fb8d9e8b41b8c527be65b
Anusha Ranganathan <anusha.ranganathan@ieee.org>
2012-12-10 09:21:44 +0000

    Changed default location

--- diskMonitorConfig.py
+++ diskMonitorConfig.py
@@ -5,7 +5,7 @@
     Copyright: (c) 2010 by Anusha Ranganathan.
 """
 
-SCAN_LOCATIONS=['/home/anusha/Documents/OULS/src/git/disk-monitor/tmp']
+SCAN_LOCATIONS=['/tmp']
 DATABASE='results.db'
 TABLE='dataOnDisk'
 FORMATS_SERVED = ["text/html", "text/xhtml", "text/plain", "application/json"]
